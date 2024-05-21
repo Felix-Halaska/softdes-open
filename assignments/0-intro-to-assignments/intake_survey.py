b2aee8afc465bf952ef872f1a52e7311e076b8e8
Felix-Halaska <felixvillage44@gmail.com>
2024-05-21 16:31:53 -0400

    Submit Assignment 0

--- assignments/0-intro-to-assignments/intake_survey.py
+++ assignments/0-intro-to-assignments/intake_survey.py
@@ -3,11 +3,11 @@ Intake survey for Software Design.
 """
 
 # How do you want us to call you? (Optionally include a pronunciation guide.)
-preferred_name = ""
+preferred_name = "Felix"
 pronunciation = ""
 
 # What are your pronouns? (Optional)
-pronouns = ""
+pronouns = "He/Him"
 
 # Define your programming experience as of this moment, on a scale of 1 to 5.
 # Here is what each number indicates:
@@ -16,17 +16,17 @@ pronouns = ""
 #   3 - Written code within the last year
 #   4 - Written a fair amount of code (e.g., a full program)
 #   5 - Done a small programming project (100-500 lines of code)
-previous_programming_experience = 0
+previous_programming_experience = 1
 
 # If necessary, please explain your programming experience.
-previous_programming_description = ""
+previous_programming_description = "Primarily MATLAB"
 
 # Define your programming experience in Python as of this moment, on a scale of
 # 1 to 5. You can use the same description for each number as above.
-previous_python_experience = 0
+previous_python_experience = 1
 
 # If necessary, please explain your Python experience.
-previous_python_description = ""
+previous_python_description = "I've downloaded Pycharm to run scripts"
 
 # Define your experience with the command line and Git as of this moment, on a
 # scale of 1 to 5. Here is what each number indicates:
@@ -35,13 +35,21 @@ previous_python_description = ""
 #   3 - Basic experience with both, AND intermediate experience with one
 #   4 - Intermediate experience with both
 #   5 - Intermediate experience with both, AND advanced experience with one
-previous_tools_experience = 0
+previous_tools_experience = 2
 
 # If necessary, please explain your experience with the command line and Git.
-previous_tools_description = ""
+previous_tools_description = (
+    "I've used the command line a little, but no git experience"
+)
 
 # What are your learning goals for this course? Please list at least one.
-learning_goals = []
+learning_goals = [
+    "achieve basic python proficiency",
+    "prepare myself for funrobo in the spring",
+]
 
 # What are you concerned about in this course? Please list at least one.
-concerns = []
+concerns = [
+    "I'm trying to do this async with no assistance",
+    "I've never really programmed before",
+]
